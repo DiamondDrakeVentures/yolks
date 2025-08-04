65beffc1cbd8c09b829097269bd5b9f722f2e0fb
GHIFARI160 <ghifari@ghifari160.com>
2025-08-04 00:23:52 -0500

    os: Add Alpine image
    Minimalistic, container native distribution. We try to use Alpine to
    keep container sizes minimal.

---
+++ os/alpine/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/ash
+
+TZ=${TZ:-UTC}
+export TZ
+
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF); exit}')
+export INTERNAL_IP
+
+cd /home/container || exit 1
+
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+
+printf "\033[38;5;62m" "$PARSED" "\033[0m"
+exec env ${PARSED}
